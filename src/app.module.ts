4dff8ef56c758f24f9bd2ce200e25fb190d945f4
Terry Chinen <terrychinen55@gmail.com>
2023-05-06 20:40:38 -0500

    update: add auth and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { NotFoundMiddleware } from './utils/middlewares/not-found.middleware';
 import { DbGraphqlConfigModule } from './config/db-graphql-config.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DbGraphqlConfigModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DbGraphqlConfigModule,
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
